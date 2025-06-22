5faa2edb9c047d5158d4969406c31f510350346e
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-06-22 19:48:46 +0000

    By notty boy

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 # config.py
 
-BOT_TOKEN = "7746062997:AAFa49yxlhKQLBXV7QkO2nNClZAkQ_hJREc"
+BOT_TOKEN = "8173629395:AAGXhau89BJpqrrVLvhZAk-hQcdEi86teLk"
 API_ID = 22947426
 API_HASH = "3ac67de232f419724b3c905e1934bc7b"
-OWNER_ID = 6135117014
-LOG_GROUP_ID = -1002709482685  # Replace with actual group ID (numeric only)
+OWNER_ID = 7168729089
+LOG_GROUP_ID = -1002023049910  # Replace with actual group ID (numeric only)
 
 # Referral + Coin Settings
 REFERRAL_COIN = 5
 REVEAL_COST = 100
 FEATURE_UNLOCK_COINS = 30
 
-# ✅ Updated MongoDB URI (for Pydroid compatibility)
+# ✅ Updated MongoDB URI 
 MONGO_URL = "mongodb+srv://jaydipmore74:xCpTm5OPAfRKYnif@cluster0.5jo18.mongodb.net/?retryWrites=true&w=majority"
 
 # Allowed genders
