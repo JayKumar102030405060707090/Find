7b6dc49a8378bfca8c7d6d51ece23eb63f473f02
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-06-23 02:49:12 +0000

    New update

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
-BOT_TOKEN = "8173629395:AAGXhau89BJpqrrVLvhZAk-hQcdEi86teLk"
-API_ID = 22947426
-API_HASH = "3ac67de232f419724b3c905e1934bc7b"
+BOT_TOKEN = "8173629395:AAFzA3dcMyvr5GAeR96RyzZFek2UDQQ5mJg"
+API_ID = 12380656
+API_HASH = "d927c13beaaf5110f25c505b7c071273"
 OWNER_ID = 7168729089
 LOG_GROUP_ID = -1002023049910  # Replace with actual group ID (numeric only)
 
@@ -15,7 +15,7 @@ FEATURE_UNLOCK_COINS = 30
 MONGO_URL = "mongodb+srv://jaydipmore74:xCpTm5OPAfRKYnif@cluster0.5jo18.mongodb.net/?retryWrites=true&w=majority"
 
 # Bot username for referral links
-BOT_USERNAME = "FindPartnerBot"  # Replace with your actual bot username
+BOT_USERNAME = "Find_partner_Robot"  # Replace with your actual bot username
 
 # Allowed genders
-GENDERS = ["Male", "Female", "Other"]"]
+GENDERS = ["Male", "Female", "Other"]
